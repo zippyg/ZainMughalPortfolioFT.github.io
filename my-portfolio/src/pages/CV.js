ff39acfa4927413defddef7ee15932823bf47331
zippyg <51121726+zippyg@users.noreply.github.com>
2024-06-23 11:45:47 +0000

    4

--- my-portfolio/src/pages/CV.js
+++ my-portfolio/src/pages/CV.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import './CV.css';
 
 const CV = () => (
@@ -10,7 +13,11 @@ const CV = () => (
       <section id="cv">
         <h2>Curriculum Vitae</h2>
         <h3>My professional background</h3>
-        <embed src="/portfolio resources/Zain Mughal NP CV - MSci .pdf" type="application/pdf" className="pdf-embed" />
+        <div className="pdf-container">
+          <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}>
+            <Viewer fileUrl="/portfolio resources/Zain Mughal NP CV - MSci .pdf" />
+          </Worker>
+        </div>
       </section>
     </main>
     <Footer />
